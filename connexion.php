3d98f82e1e5e46e5db9a588675f0885fe1746294
ESSADEK Salim <salim.essadek@gmail.com>
2021-01-05 16:50:03 +0100

    connexion php

--- connexion.php
+++ connexion.php
@@ -6,6 +6,7 @@ $db = "gestion_projet";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
 }
 catch(PDOException $e)
 {
